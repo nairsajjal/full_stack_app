cb09c6acf2588ae405fc3fc80438136c905f1b2c
sajjal <nairsajjal@gmail.com>
2019-07-05 10:34:33 +0530

    commit before deleting

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send({express:"" });
-})
+app.get('/api/hello',(req,res)=>{
+    res.send({express:"Hello" });
+});
+app.post('/api/world', (req, res) => {
+    console.log(req.body);
+    res.send(
+      `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
+  });
+  
+
 app.listen(port,()=>{console.log(`listening on port ${port}`)});
